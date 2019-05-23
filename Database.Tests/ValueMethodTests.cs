572132362878aedc6757e9897ae59cd1b75fc60e
Toni Gottschall <toni.gottschall@nightlycode.de>
2019-05-23 17:16:58 +0200

    added more async tests

--- Database.Tests/ValueMethodTests.cs
+++ Database.Tests/ValueMethodTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using NightlyCode.Database.Clients;
 using NightlyCode.Database.Entities;
 using NightlyCode.Database.Entities.Operations.Fields;
@@ -9,10 +10,10 @@ using NUnit.Framework;
 
 namespace NightlyCode.Database.Tests {
 
-    [TestFixture]
+    [TestFixture, Parallelizable]
     public class ValueMethodTests {
 
-        [Test]
+        [Test, Parallelizable]
         public void LoadSet() {
             IDBClient dbclient = TestData.CreateDatabaseAccess();
             EntityManager entitymanager = new EntityManager(dbclient);
@@ -27,7 +28,23 @@ namespace NightlyCode.Database.Tests {
             Assert.True(new[] {0, 1, 2, 3, 4}.SequenceEqual(result));
         }
 
-        [Test]
+        [Test, Parallelizable]
+        public async Task LoadSetAsync()
+        {
+            IDBClient dbclient = TestData.CreateDatabaseAccess();
+            EntityManager entitymanager = new EntityManager(dbclient);
+
+            entitymanager.UpdateSchema<ValueModel>();
+
+            PreparedOperation operation = entitymanager.Insert<ValueModel>().Columns(v => v.Integer, v => v.Single, v => v.Double).Prepare();
+            for (int i = 0; i < 5; ++i)
+                operation.Execute(i, 0.0f, 0.0);
+
+            int[] result = (await entitymanager.Load<ValueModel>(v => v.Integer).ExecuteSetAsync<int>()).ToArray();
+            Assert.True(new[] { 0, 1, 2, 3, 4 }.SequenceEqual(result));
+        }
+
+        [Test, Parallelizable]
         public void LoadScalar()
         {
             IDBClient dbclient = TestData.CreateDatabaseAccess();
@@ -40,7 +57,20 @@ namespace NightlyCode.Database.Tests {
             Assert.AreEqual(4, entitymanager.Load<ValueModel>(v => v.Integer).ExecuteScalar<int>());
         }
 
-        [Test]
+        [Test, Parallelizable]
+        public async Task LoadScalarAsync()
+        {
+            IDBClient dbclient = TestData.CreateDatabaseAccess();
+            EntityManager entitymanager = new EntityManager(dbclient);
+
+            entitymanager.UpdateSchema<ValueModel>();
+
+            entitymanager.Insert<ValueModel>().Columns(v => v.Integer, v => v.Single, v => v.Double).Values(4, 0.0f, 0.0).Execute();
+
+            Assert.AreEqual(4, await entitymanager.Load<ValueModel>(v => v.Integer).ExecuteScalarAsync<int>());
+        }
+
+        [Test, Parallelizable]
         public void DeleteAll() {
             IDBClient dbclient = TestData.CreateDatabaseAccess();
             EntityManager entitymanager = new EntityManager(dbclient);
@@ -55,7 +85,23 @@ namespace NightlyCode.Database.Tests {
             Assert.AreEqual(0, entitymanager.Load<ValueModel>(m => DBFunction.Count).ExecuteScalar<int>());
         }
 
-        [Test]
+        [Test, Parallelizable]
+        public async Task DeleteAllAsync()
+        {
+            IDBClient dbclient = TestData.CreateDatabaseAccess();
+            EntityManager entitymanager = new EntityManager(dbclient);
+
+            entitymanager.UpdateSchema<ValueModel>();
+
+            PreparedOperation operation = entitymanager.Insert<ValueModel>().Columns(v => v.Integer, v => v.Single, v => v.Double).Prepare();
+            for (int i = 0; i < 5; ++i)
+                operation.Execute(i, 0.0f, 0.0);
+
+            entitymanager.Delete<ValueModel>().Execute();
+            Assert.AreEqual(0, await entitymanager.Load<ValueModel>(m => DBFunction.Count).ExecuteScalarAsync<int>());
+        }
+
+        [Test, Parallelizable]
         public void DeleteSingle()
         {
             IDBClient dbclient = TestData.CreateDatabaseAccess();
@@ -74,7 +120,26 @@ namespace NightlyCode.Database.Tests {
             Assert.False(values.Any(v => v == 3));
         }
 
-        [Test]
+        [Test, Parallelizable]
+        public async Task DeleteSingleAsync()
+        {
+            IDBClient dbclient = TestData.CreateDatabaseAccess();
+            EntityManager entitymanager = new EntityManager(dbclient);
+
+            entitymanager.UpdateSchema<ValueModel>();
+
+            PreparedOperation operation = entitymanager.Insert<ValueModel>().Columns(v => v.Integer, v => v.Single, v => v.Double).Prepare();
+            for (int i = 0; i < 5; ++i)
+                operation.Execute(i, 0.0f, 0.0);
+
+            entitymanager.Delete<ValueModel>().Where(v => v.Integer == 3).Execute();
+
+            int[] values = (await entitymanager.Load<ValueModel>(v => v.Integer).ExecuteSetAsync<int>()).ToArray();
+            Assert.AreEqual(4, values.Length);
+            Assert.False(values.Any(v => v == 3));
+        }
+
+        [Test, Parallelizable]
         public void DeleteTransactionRollback() {
             IDBClient dbclient = TestData.CreateDatabaseAccess();
             EntityManager entitymanager = new EntityManager(dbclient);
@@ -92,5 +157,27 @@ namespace NightlyCode.Database.Tests {
 
             Assert.AreEqual(5, entitymanager.Load<ValueModel>(m => DBFunction.Count).ExecuteScalar<int>());
         }
+
+        [Test, Parallelizable]
+        public async Task DeleteTransactionRollbackAsync()
+        {
+            IDBClient dbclient = TestData.CreateDatabaseAccess();
+            EntityManager entitymanager = new EntityManager(dbclient);
+
+            entitymanager.UpdateSchema<ValueModel>();
+
+            PreparedOperation operation = entitymanager.Insert<ValueModel>().Columns(v => v.Integer, v => v.Single, v => v.Double).Prepare();
+            for (int i = 0; i < 5; ++i)
+                await operation.ExecuteAsync(i, 0.0f, 0.0);
+
+            using (Transaction transaction = entitymanager.Transaction())
+            {
+                await entitymanager.Delete<ValueModel>().ExecuteAsync(transaction);
+                transaction.Rollback();
+            }
+
+            Assert.AreEqual(5, await entitymanager.Load<ValueModel>(m => DBFunction.Count).ExecuteScalarAsync<int>());
+        }
+
     }
 }
