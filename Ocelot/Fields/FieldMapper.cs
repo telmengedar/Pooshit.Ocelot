746e1f3da6db89a83419536ccf5f068f877e844d
Toni <toni.gottschall@nightlycode.eu>
2024-10-20 12:52:21 +0200

    Update FieldMapper initializer and add EntityFromTable
    Enhanced FieldMapper to accept a function for field value extraction in the initializer. Added IndexOf helper method for field indexing and introduced EntityFromTable for creating a single entity from a DataTable. Upgraded project version to 0.14.3-preview.

--- Ocelot/Fields/FieldMapper.cs
+++ Ocelot/Fields/FieldMapper.cs
@@ -26,7 +26,7 @@ public class FieldMapper<TEntity> {
     /// </summary>
     /// <param name="mappings">mappings to initialize mapper with</param>
     /// <param name="initializer">initializer used to initialize entities before processing mappings</param>
-    public FieldMapper(FieldMapping<TEntity>[] mappings, Action<TEntity, string[]> initializer=null) 
+    public FieldMapper(FieldMapping<TEntity>[] mappings, Action<TEntity, string[], Func<string, object>> initializer=null) 
     : this((IEnumerable<FieldMapping<TEntity>>)mappings, initializer)
     {
     }
@@ -36,7 +36,7 @@ public class FieldMapper<TEntity> {
     /// </summary>
     /// <param name="initializer">initializer used to initialize entities before processing mappings</param>
     /// <param name="mappings">mappings to initialize mapper with</param>
-    public FieldMapper(IEnumerable<FieldMapping<TEntity>> mappings, Action<TEntity, string[]> initializer=null) {
+    public FieldMapper(IEnumerable<FieldMapping<TEntity>> mappings, Action<TEntity, string[], Func<string, object>> initializer=null) {
         this.mappings.AddRange(mappings);
         InitializeEntity = initializer;
         BuildFieldLookup();
@@ -53,7 +53,7 @@ public class FieldMapper<TEntity> {
     /// </summary>
     public IEnumerable<IDBField> DbFields => mappings.Select(m => m.Field);
 
-    Action<TEntity, string[]> InitializeEntity { get; }
+    Action<TEntity, string[], Func<string,object>> InitializeEntity { get; }
     
     void BuildFieldLookup() {
         fieldLookup = new();
@@ -79,6 +79,17 @@ public class FieldMapper<TEntity> {
         return names.Select(n => this[n].Field);
     }
 
+    int IndexOf(string[] fields, string fieldName) {
+        int index = 0;
+        foreach (string field in fields) {
+            if (field == fieldName)
+                return index;
+            ++index;
+        }
+
+        return -1;
+    }
+    
     /// <summary>
     /// creates an entity from a loaded row
     /// </summary>
@@ -87,7 +98,7 @@ public class FieldMapper<TEntity> {
     /// <returns>created entity</returns>
     public TEntity EntityFromRow(DataRow row, params string[] fields) {
         TEntity entity = Activator.CreateInstance<TEntity>();
-        InitializeEntity?.Invoke(entity, fields);
+        InitializeEntity?.Invoke(entity, fields, fieldName => row.GetValue<object>(IndexOf(fields, fieldName)));
         int index = 0;
         if (fields.Length == 0) {
             foreach (FieldMapping<TEntity> field in mappings)
@@ -109,6 +120,16 @@ public class FieldMapper<TEntity> {
     /// <returns>enumeration of entities</returns>
     public IEnumerable<TEntity> EntitiesFromTable(DataTable table, params string[] fields) {
         return table.Rows.Select(r=>EntityFromRow(r, fields));
+    }
+
+    /// <summary>
+    /// create entities from table
+    /// </summary>
+    /// <param name="table">table from which to create entities</param>
+    /// <param name="fields">expected fields in rows (optional)</param>
+    /// <returns>enumeration of entities</returns>
+    public TEntity EntityFromTable(DataTable table, params string[] fields) {
+        return table.Rows.Select(r => EntityFromRow(r, fields)).FirstOrDefault();
     }
 
 }
